5097783ccf264782dd7f53c8d7c9ce1d1c35828c
mflorezr <mflorezr@eafit.edu.co>
2019-10-26 00:52:05 -0500

    Doolittle terminado y Piv Parcial modificado

--- Mathnalysis/PivParcial.cpp
+++ Mathnalysis/PivParcial.cpp
@@ -5,32 +5,44 @@
 
 using namespace std;
 
+void toStringIn(vector<vector<double> > &matrix) {
+  for (unsigned int i = 0; i < matrix.size(); i++)
+	{
+		for (unsigned int j = 0; j < matrix[0].size(); j++)
+		{
+			printf("%f ", matrix[i][j]);
+		}
+		cout << endl;
+	}
+	printf("\n");
+}
+
 vector<vector<double>> swapRows(vector<vector<double>> Ab, int filaMayor, int k){
-  int n= Ab.size();
-    vector<double> aux = Ab[filaMayor];
-    
+  vector<double> aux = Ab[k];  
     Ab[k]=Ab[filaMayor];
     Ab[filaMayor]=aux;
-    
     return Ab;
 }
 
 vector<vector <double> > pivParcial (vector<vector<double>> &Ab, int n, int k){
-  double mayor = Ab[k][k];
-  double filaMayor = k;
+  double mayor = abs(Ab[k][k]);
+  double filaMayor=0;
   for(int s=k+1;s<n; s++){
+    filaMayor=k;
     if (abs(Ab[s][k]) > mayor){
-      mayor=Ab[s][k];
+      mayor=abs(Ab[s][k]);
       filaMayor=s;
     }
     if(mayor==0){
+      cout << "El sistema no tiene solución unica" << endl;
     } else {
       if(filaMayor != k){
-	Ab=swapRows(Ab,filaMayor,k); 
+	cout << "Filas " << k << " y " << filaMayor << " cambiadas" << endl;
+	Ab=swapRows(Ab,filaMayor,k);
       }
-    }
-    
-  }
+    }    
+  } 
+  toStringIn(Ab);
   return Ab;
 }
 
@@ -46,79 +58,87 @@ vector<vector<double> > formaMatrizAumentada(vector<vector<double> > a, vector<d
     }
 
     return aumentada;
+    
 }
 
-vector <double> EliminacionGauss(vector<vector<double>> &A, vector<double> b, int n){
-  vector<double> x(n, 0.0);
-  vector<vector<double> > Ab(n, vector<double>(n+1, 0.0));
-  Ab = formaMatrizAumentada(A,b,n);
-  
-}
-
-
-vector<vector<double>> EliminacionGaussPivoteo(vector<vector<double> > &Ab,int n){
-  for(int k =0; k<n-1; k++){
-    Ab = pivParcial(Ab,n,k);
-    double multiplicador;
-    for(int i=k+1; i<n; i++){
-      multiplicador = (Ab[i][k]/Ab[k][k]);
-      for(int j=k; j<n+1; j++){
-	Ab[i][j]=(Ab[i][j]-(multiplicador*Ab[k][j]));
-      }
-    }
-  }
-  return Ab;
-}
-
-  vector<double> sustitucionBackward(vector<vector<double> > Ab, int n) {
+vector<double> sustitucionBackward(vector<vector<double> > Ab, int n) {
     vector<double> x(n, 0.0);
-    x[n]= (Ab[n][n+1]/Ab[n][n]);
+    x[n]= (Ab[n-1][n]/Ab[n-1][n-1]);
 	for (int i = n - 1; i >= 0; i--) {
 	  double sum = 0;
 	  for (int j = i + 1; j < n; j++) {
-	    sum += sum+(Ab[i][j]*x[j]);
+	    sum = sum+(Ab[i][j]*x[j]);
 	  }
 	  double denominator = Ab[i][i];
 	  if (denominator == 0) {
-	    printf("ERROR");
+	    printf("Error por division por 0");
 	  }
 	  else {
-	    x[i] = (Ab[i][n+1] - sum) / Ab[i][i];
+	    x[i] = (Ab[i][n] - sum) / Ab[i][i];
 	  }
 	}
 	return x;
 }
 
-
-  void toStringIn(vector<vector<double> > &matriz, char name) {
-	printf("%c matriz\n", name);
-
-	for (unsigned int i = 0; i < matriz.size(); i++){
-		for (unsigned int j = 0; j < matriz[0].size(); j++) {
-			if (matriz[i][j] == DBL_MAX) {
-				printf("%6c%d%d ", name, j + 1, i + 1);
-			}
-			else {
-				printf("%.6f ", matriz[i][j]);
-			}
-		}
-		cout << endl;
-	}
-	printf("\n");
+vector<vector<double>> EliminacionGaussPivoteo(vector<vector<double> > &Ab,int n){
+  for(int k =0; k<n-1; k++){
+    cout<< "ETAPA " << k+1 << endl;
+    Ab = pivParcial(Ab,n,k);
+    double multiplicador=0;
+    for(int i=k+1; i<n; i++){
+      multiplicador = (Ab[i][k]/Ab[k][k]);
+      //cout << multiplicador << endl;
+      for(int j=k; j<n+1; j++){
+	Ab[i][j]=(Ab[i][j]-(multiplicador*Ab[k][j]));
+	
+      }
+    }
+   toStringIn(Ab);
+  }
+  
+  return Ab;
 }
 
-  int main() {
-  vector<vector<double> > l(2, vector<double>(2, 0.0));
-  l[0][0] = 1;
-  l[0][1] = 2;
-  l[1][0] = 3;
-  l[1][1] = 4;
-  
-  vector<double> m(2, 3.4);
-  
-  vector<vector<double> > a = formaMatrizAumentada(l,m,2);
 
-  toStringIn(a,'a');
+
+vector <double> EliminacionGauss(vector<vector<double>> &A, vector<double> b, int n){
+  vector<double> x(n, 0.0);
+  vector<vector<double> > Ab(n, vector<double>(n+1, 0.0));
+  Ab = formaMatrizAumentada(A,b,n);
+  Ab = EliminacionGaussPivoteo(Ab,n);
+  x= sustitucionBackward(Ab, n);
+  return x;
+}
+
+
+  int main() {
+  vector<vector<double> > l(4, vector<double>(4, 0.0));
+  l[0][0] = -7;
+  l[0][1] = 2;
+  l[0][2] = -3;
+  l[0][3] = 4;
+  l[1][0] = 5;
+  l[1][1] = -1;
+  l[1][2] = 14;
+  l[1][3] = -1;
+  l[2][0] = 1;
+  l[2][1] = 9;
+  l[2][2] = -7;
+  l[2][3] = 5;
+  l[3][0] = -12;
+  l[3][1] = 13;
+  l[3][2] = -8;
+  l[3][3] = -4;
+  
+  vector<double> m={-12,13,31,-32};
+  
+  vector<double>  a = EliminacionGauss(l,m,4);
+
+  for(int i =0; i< a.size(); i++){
+    cout <<"x"<<i+1<<"= "<< a[i] << endl;
+
+  }
+  // toStringIn(a,'a');
   
 }
 
