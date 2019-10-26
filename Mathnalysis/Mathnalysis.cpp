0f4d6cd7bda81e86f9ce42970a07f67b4762f721
Eduard <yamiyugi43@gmail.com>
2019-10-25 19:13:56 -0500

    bugs

--- Mathnalysis/Mathnalysis.cpp
+++ Mathnalysis/Mathnalysis.cpp
@@ -6,6 +6,13 @@
 #include "exceptions.h"
 #include "PuntoFijo.h"
 #include "raices_multiples.h"
+#include "biseccion.h"
+#include "busqueda_incrementa.h"
+#include "newton.h"
+#include "reglaFalsa.h"
+#include "secante.h"
+#include "gaussSeidel.h"
+#include "crout.h"
 #include <math.h>
 #include <stdio.h>
 #include <iostream>
@@ -19,8 +26,11 @@ int main()
 {
 	try {
 		//double x = puntoFijo(f, g, -0.5, 11, 0.0005, 0);
-		double x = raicesMultiples(f,g,g2,-3.2,11,0.0005,0);
-		cout << x << endl;
+		//double x = raicesMultiples(f,g,g2,-3.2,11,0.0005,0);
+		vector<vector<double> > A = { {20,-1,3,4} , {6,23,4,3} , {7,21,46,9} , {-3,-9,12,38}};
+		vector<double> b = { 30,-10,20,-14 };
+		vector<double> c = { 0,0,0,0 };
+		metodoGaussSeidel(c,A,b);
 	}
 	catch (exception e) {
 		cout << "gg" << endl;
