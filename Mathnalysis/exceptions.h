bc7c708fde2e6519c77e8e3f668dfffe078b644a
Eduard <yamiyugi43@gmail.com>
2019-10-28 20:50:45 -0500

    excepciones recuperadas

---
+++ Mathnalysis/exceptions.h
@@ -0,0 +1,44 @@
+#include <exception>
+
+/**
+* Excepsion para cuando se supere el numero total de iteraciones
+*/
+
+struct TooMuchIterException : public std::exception {
+	const char* what() const noexcept {
+		return "No se pudo Encontrar la raiz con la cantidad de iteraciones pedido";
+	}
+};
+
+/**
+* Excepsion para cuando el intervalo no sea valido
+*/
+
+struct BadIntervalException : public std::exception {
+
+	const char* what() const noexcept {
+		return "Intervalo invalido, por favor inserte otro";
+	}
+};
+
+/**
+* Excepsion para cuando la derivada es 0
+*/
+
+struct Derived0Exception : public std::exception {
+
+	const char* what() const noexcept {
+		return "Derivada igual a 0";
+	}
+};
+
+/**
+* Excepsion para cuando se intenta dividir por 0
+*/
+
+struct div0Exception : public std::exception {
+
+	const char* what() const noexcept {
+		return "Se intento divir por 0";
+	}
+};
