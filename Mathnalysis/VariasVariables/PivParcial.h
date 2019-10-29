56406a08705d8f1d202406d950004afe29d97268
mflorezr <mflorezr@eafit.edu.co>
2019-10-29 15:32:29 -0500

    correccion pivparcial.h

--- Mathnalysis/VariasVariables/PivParcial.h
+++ Mathnalysis/VariasVariables/PivParcial.h
@@ -11,5 +11,5 @@ vector<vector<double>> swapRows(vector<vector<double>> Ab, int filaMayor, int k)
 vector<vector <double> > pivParcial (vector<vector<double>> &Ab, int n, int k);
 vector<vector<double> > formaMatrizAumentada(vector<vector<double> > a, vector<double> b, int n);
 vector<double> sustitucionBackward(vector<vector<double> > Ab, int n);
-vector<vector<double>> EliminacionGaussPivoteo(vector<vector<double> > &Ab,int n)
+vector<vector<double>> EliminacionGaussPivoteo(vector<vector<double> > &Ab,int n);
 vector <double> EliminacionGauss(vector<vector<double>> &A, vector<double> b, int n);
