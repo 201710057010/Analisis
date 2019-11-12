53202e7d16779246fc9afa525f967ce65461950f
Eduard <yamiyugi43@gmail.com>
2019-11-11 19:46:00 -0500

    cambios menores

--- Mathnalysis/VariasVariables/PivParcial.h
+++ Mathnalysis/VariasVariables/PivParcial.h
@@ -12,4 +12,4 @@ vector<vector <double> > pivParcial (vector<vector<double> > &Ab, int n, int k);
 vector<vector<double> > formaMatrizAumentadapiv(vector<vector<double> > a, vector<double> b, int n);
 vector<double> sustitucionBackwardPiv(vector<vector<double> > Ab, int n);
 vector<vector<double> > EliminacionGaussPivoteo(vector<vector<double> > &Ab,int n);
-vector <double> EliminacionGauss(vector<vector<double>> &A, vector<double> b, int n);
+vector <double> EliminacionGauss(vector<vector<double> > &A, vector<double> b, int n);
--- Mathnalysis/VariasVariables/PivTotal.h
+++ Mathnalysis/VariasVariables/PivTotal.h
@@ -10,6 +10,6 @@ void toStringInPivT(vector<vector<double> > &matrix);
 vector<vector<double> > swapColPivT(vector<vector<double> > Ab, int colMayor, int k);
 vector<vector <double> > pivTotal (vector<vector<double> > &Ab, int n, int k);
 vector<vector<double> > formaMatrizAumentadaPivT(vector<vector<double> > a, vector<double> b, int n);
-vector<vector<double>> EliminacionGaussPivT(vector<vector<double> > &Ab,int n);
+vector<vector<double> > EliminacionGaussPivT(vector<vector<double> > &Ab,int n);
 vector<double> sustitucionBackwardPivt(vector<vector<double> > Ab, int n);
 vector <double> EliminacionGaussPivT(vector<vector<double> > &A, vector<double> b, int n);
